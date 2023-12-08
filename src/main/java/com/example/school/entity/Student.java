6dc00658843c483ab09475e657b7e3ba79d5cf2e
Toan <hongtoan20042000@gmail.com>
2023-12-08 15:48:55 +0700

    first commit

---
+++ src/main/java/com/example/school/entity/Student.java
@@ -0,0 +1,26 @@
+package com.example.school.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="tbl_student")
+public class Student {
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+
+}
